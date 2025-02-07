af14a97c6592340e8f15fb7da8e22f8fe742ffc6
Laura Berge <lauraeberge@gmail.com>
2025-02-07 16:53:19 -0600

    solution

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [query, setQuery] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(r => r.json())
+    .then(data => setPlants(data))
+  }, [])
+
+  const filteredPlants = query ? plants.filter(p => p.name.toLowerCase().includes(query.toLowerCase())) : plants
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants} />
+      <Search setQuery={setQuery} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
